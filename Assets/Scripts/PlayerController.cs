0a1245b225ff04ec82b01029e8ed802f0b8faa2f
D4rkN00dl3s <martin.osit.2057@gmail.com>
2025-04-19 00:39:31 +0300

    Added player and basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    [Range(1, 20)]
+    [SerializeField] private uint verticalSpeed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovementHandler();
+    }
+
+    void MovementHandler()
+    {
+        float input = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, input * verticalSpeed * Time.deltaTime, 0);
     }
 }
